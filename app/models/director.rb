55a1c5317bc3c74d6836938e80784769f3a1d745
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-10-02 17:06:00 +0000

    created 'has_many' association for actor called 'filmography'

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,7 +2,7 @@
 #
 # Table name: directors
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  bio        :text
 #  dob        :date
 #  image      :string
@@ -12,12 +12,8 @@
 #
 class Director < ApplicationRecord
   validates(:name, presence: true)
+  has_many(:movies, class_name:"Movie", foreign_key:"director_id")
+  
+  has_many(:filmography, class_name: "Movie", foreign_key: "director_id")
 
-  def filmography
-    key = self.id
-
-    the_many = Movie.where({ :director_id => key })
-
-    return the_many
-  end
 end
